c52efa973a487bfb2d95dc67df4ad6b49e4c00be
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2021-08-16 21:03:02 -0500

    chore: added keyboard events "movements and collision"

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,5 +1,6 @@
 import hamburgerMenu from "./hamburger_menu_dom.js";
 import { alarm, digitalClick } from "./reloj_digital_y_alarma_sonora.js";
+import { moveBall, shortcuts } from "./teclado.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -7,3 +8,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
   digitalClick("#reloj","#active-reloj", "#desactive-reloj");
   alarm("../assets/alarma.mp3", "#active-alarm", "#desactive-alarm");
 });
+
+d.addEventListener("keydown", (e)=>{
+  shortcuts(e);
+  moveBall(e, ".ball", ".stage");
+})
--- js/teclado.js
+++ js/teclado.js
@@ -1,4 +1,45 @@
 const d = document;
+let x = 0,
+  y = 0;
+
+export function moveBall(e, ball, stage) {
+  const $ball = d.querySelector(ball),
+    $stage = d.querySelector(stage),
+    limitBall = $ball.getBoundingClientRect(),
+    limitStage = $stage.getBoundingClientRect();
+  console.log(e.keyCode);
+  console.log(e.key);
+
+  switch (e.keyCode) {
+    case 37:
+      if(limitBall.left > limitStage.left){
+        e.preventDefault();
+        x--;
+      }
+      break;
+    case 38:
+      if(limitBall.top > limitStage.top){
+      e.preventDefault();
+      y--;
+    }
+      break;
+    case 39:
+      if(limitBall.right < limitStage.right){
+      x++;
+      e.preventDefault();
+      }
+      break;
+    case 40:
+      if(limitBall.bottom < limitStage.bottom){
+      e.preventDefault();
+      y++;
+      }
+      break;
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x * 10}px, ${y * 10}px)`;
+}
 
 export function shortcuts(e) {
   console.log(e.type);
@@ -9,12 +50,12 @@ export function shortcuts(e) {
   console.log(e);
 
   if (e.key === "a" && e.altKey) {
-    alert("Haz lanzado una alerta con el teclado")
+    alert("Haz lanzado una alerta con el teclado");
   }
   if (e.key === "c" && e.altKey) {
-    alert("Haz lanzado una confirmación con el teclado")
+    alert("Haz lanzado una confirmación con el teclado");
   }
   if (e.key === "p" && e.altKey) {
-    alert("Haz lanzado una aviso con el teclado")
+    alert("Haz lanzado una aviso con el teclado");
   }
 }
