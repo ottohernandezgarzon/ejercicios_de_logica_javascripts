fb877657fc27e25fa94aaf80d7ca52139966e70c
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2021-08-16 19:12:20 -0500

    Chore: added keyboard events="key, ctrlKey and altKey"

--- js/teclado.js
+++ js/teclado.js
@@ -1,4 +1,20 @@
 const d = document;
 
 export function shortcuts(e) {
+  console.log(e.type);
+  console.log(e.key);
+  console.log(e.keyCode);
+  console.log(e.ctrlKey);
+  console.log(e.altKey);
+  console.log(e);
+
+  if (e.key === "a" && e.altKey) {
+    alert("Haz lanzado una alerta con el teclado")
+  }
+  if (e.key === "c" && e.altKey) {
+    alert("Haz lanzado una confirmación con el teclado")
+  }
+  if (e.key === "p" && e.altKey) {
+    alert("Haz lanzado una aviso con el teclado")
+  }
 }
