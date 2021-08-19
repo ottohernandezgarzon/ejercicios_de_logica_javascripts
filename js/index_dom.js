668a89b0ea0e7aad939e99d25a52e5938e7552e4
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2021-08-18 20:22:53 -0500

    Chore: JavaScript exercise "countdown"

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,3 +1,4 @@
+import countdown from "./cuenta_regresiva.js";
 import hamburgerMenu from "./hamburger_menu_dom.js";
 import { alarm, digitalClick } from "./reloj_digital_y_alarma_sonora.js";
 import { moveBall, shortcuts } from "./teclado.js";
@@ -7,6 +8,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClick("#reloj","#active-reloj", "#desactive-reloj");
   alarm("../assets/alarma.mp3", "#active-alarm", "#desactive-alarm");
+  countdown("countdown", "Aug 18, 2021, 20:14:00", "Feliz cumple años");
 });
 
 d.addEventListener("keydown", (e)=>{
