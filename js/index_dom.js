73d97f6fd24d52c816e0deb8b4d8820de2d694e2
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2021-08-21 18:41:29 -0500

    chore: added theme darkMode or lightMode

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import { alarm, digitalClick } from "./reloj_digital_y_alarma_sonora.js";
 import { moveBall, shortcuts } from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -10,10 +11,12 @@ d.addEventListener("DOMContentLoaded", (e) => {
   digitalClick("#reloj", "#active-reloj", "#desactive-reloj");
   alarm("../assets/alarma.mp3", "#active-alarm", "#desactive-alarm");
   countdown("countdown", "Aug 18, 2021, 20:14:00", "Feliz cumple años");
-  scrollTopButton(".scroll-top-btn")
+  scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
   shortcuts(e);
   moveBall(e, ".ball", ".stage");
 });
+
+  darkTheme(".dark-theme-btn", "dark-mode");
