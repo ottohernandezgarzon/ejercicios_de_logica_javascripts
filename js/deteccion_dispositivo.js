16c7888b423b9fdb47ccb44bd2217bff8863d9e1
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2021-08-26 19:20:33 -0500

    chore:  device discovery (user agent)

---
+++ js/deteccion_dispositivo.js
@@ -0,0 +1,42 @@
+const d = document,
+  n = navigator,
+  ua = n.userAgent;
+
+export default function userDeviceInfo(id) {
+  const $id = d.getElementById(id),
+    isMobile = {
+      android: () => ua.match(/android/i),
+      ios: () => ua.match(/¡phone|¡pad|¡pod/i),
+      windows: () => ua.match(/windows phone/i),
+      any: function() {
+        return this.android() || this.ios() || this.windows();
+      }
+    },
+    isDesktop = {
+      linux: () => ua.match(/linux/i),
+      mac: () => ua.match(/mac os/i),
+      windows: () => ua.match(/windows nt/i),
+      any: function() {
+        return this.linux() || this.mac() || this.windows
+      }
+    },
+    isBrowser = {
+      chrome: ua.match(/chrome/i),
+      safari: ua.match(/safari/i),
+      firefox: ua.match(/firefox/i),
+      opera: ua.match(/opera||opera mini/i),
+      ie: ua.match(/msie||iemobile/i),
+      edge: ua.match(/edge/i),
+      any: function() {
+        return (this.chrome() || this.safari() || this.firefox() || this.opera() || this.ie() || this.edge)
+      }
+    };
+  $id.innerHTML = `
+  <ul>
+    <li> User Agent: <b> ${ua}</b></li>
+    <li>Plataforma:  <b> ${
+      isMobile.any()? isMobile.any():isDesktop.any()
+    }</b></li>
+    <li> Navegador: <b>${isBrowser.any()}</b></li>
+  </ul>`
+}
