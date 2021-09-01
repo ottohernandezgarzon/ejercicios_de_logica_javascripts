c5c6cb30f34be52c67a3a6606b7a52c60a9983e7
ottohernandezgarzon <ohernandezgarzon@gmail.com>
2021-09-01 16:34:48 -0500

    perf: exercicie correction

--- js/decteccion_red.js
+++ js/decteccion_red.js
@@ -7,17 +7,17 @@ export default function networkStatus() {
     const $div =d.createElement("div");
 
     if (n.onLine) {
-      $div.textContent = "conexion restablecida";
+      $div.textContent = "conexión restablecida";
       $div.classList.add("online");
-      $div.classList.remove("offine")
+      $div.classList.remove("offline")
     } else {
-      $div.textContent = "conexion restablecida";
-      $div.classList.add("offine");
+      $div.textContent = "conexión perdida";
+      $div.classList.add("offline");
       $div.classList.remove("online");
     }
     d.body.insertAdjacentElement("afterbegin", $div);
     setTimeout(() => d.body.removeChild($div), 2000);
   }
   w.addEventListener("online", (e) => isOnline());
-  w.addEventListener("online", (e) => isOnline());
+  w.addEventListener("offline", (e) => isOnline());
 }
